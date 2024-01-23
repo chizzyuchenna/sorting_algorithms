2b9c01aa395c9a9a11fe3ee89c72e4404a163fdf
chizzyuchenna <uchennachizua@gmail.com>
2024-01-23 14:26:16 -0800

    task5

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,48 +1,35 @@
-/*
- * File_Name: 0-bubble_sort.c
- * Created: 29th June, 2023
- * Author: David James Taiye (Official0mega)
- * Size_Of_File: Undefined
- * Project_Title: sorting_algorithms
- * Status: Submitted.!
- */
-
-
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- *               using the bubble sort algorithm.
- * @array: Pointer to the array to be sorted.
- * @size: The number of elements in the array.
+ * bubble_sort - sorts an array of integers in ascending order
  *
- * Description: This function implements the bubble sort algorithm
- *              to sort an array of integers in ascending order.
- *              It compares adjacent elements and swaps them if they
- *              are in the wrong order until the entire array is sorted.
+ * @array: The array of integer
+ * @size: The size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp;
+	size_t i;
+	int tmp, sorted;
 
-	if (array == NULL || size < 2)
+	if (!array || size == 0)
 		return;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size - 1; j++)
+	do {
+		sorted = 1;
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j + 1] < array[j])
+			if (array[i] > array[i + 1])
 			{
-				/* Swap adjacent elements */
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-
-				/* Print the current state of the array */
+				sorted = 0;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
 				print_array(array, size);
 			}
 		}
-	}
+
+	} while (sorted == 0);
 }
